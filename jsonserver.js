f30a503361710cce9697fee1ced7756a498bf7bf
manojsk000 <manoj.mm867@gmail.com>
2023-06-26 18:37:49 +0530

    updated

--- jsonserver.js
+++ jsonserver.js
@@ -1,10 +1,12 @@
 const jsonServer = require("json-server");
 const server = jsonServer.create();
-const router = jsonServer.router("movies.json");
+const router1 = jsonServer.router("movies.json");
+const router2 = jsonServer.router("portfolio.json")
 const middlewares = jsonServer.defaults();
 const port = process.env.PORT || 8892;
 
 server.use(middlewares);
-server.use(router);
+server.use(router1);
+server.use(router2)
 
 server.listen(port);
